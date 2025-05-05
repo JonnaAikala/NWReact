1e24d9f2be4f30926c372587e62dd4d0be229ae2
Jonna Wiro <jonna.wiro@student.careeria.fi>
2025-05-05 18:30:16 +0300

    customer details ok

---
+++ src/CustomerList.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+import React, {useState, useEffect} from 'react'
+import CustomerService from './services/Customer'
+import Customer from './Customer'
+
+// Propsi otettu vastaan suoraan nimellä
+const CustomerList = () => {
+
+    // Komponentin tilan määritys
+    const [customers, setCustomers] = useState([])
+    const [showCustomers, setShowCustomers] = useState(false)
+
+useEffect(() => {
+    CustomerService.getAll()
+    .then(data => {
+        setCustomers(data)
+    })
+},[]
+)
+
+
+  return (
+      <>
+            <h2 onClick={() => setShowCustomers(!showCustomers)}>Customers</h2>
+       
+            {
+                showCustomers && customers && customers.map(c => (
+                    <Customer key={c.customerId} customer={c}/>
+                )
+
+                )
+            }
+      </>
+  )
+}
+
+export default CustomerList
