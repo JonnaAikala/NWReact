b69f745c0f9f83db41aecdb55edbaeda3c40a617
JonnaAikala <jonna.aikala1@gmail.com>
2025-09-22 16:09:23 +0300

    Update Auth.js

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const loginUrl = "https://northwindrestapijonnaaikala-eycfd4c8c6hggugu.swedencentral-01.azurewebsites.net"
+const loginUrl = "https://northwindrestapijulkaistu-d6gffbe5bcc3drdu.swedencentral-01.azurewebsites.net"
 
 const Login = (object) => {
     const request = axios.post(loginUrl, object)
